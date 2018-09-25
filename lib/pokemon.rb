0263f2bc47a54d537550574def82eafc2ac9831d
Ed Putans <edputans@gmail.com>
2018-09-25 10:50:13 +0100

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,36 @@
+
+require "pry"
+
 class Pokemon
+
+
+  @@all =[]
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:id, name:name,type:type,db:db)
+    @id=id
+    @name=name
+    @type=type
+    @db=db
+    #binding.pry
+  end
+
+def self.all
+  @@all
+end
+
+def self.save(name,type,db)
+db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)",name,type)
+#binding.pry
+end
+
+def self.find(id,db)
+  sql = "SELECT * FROM pokemon WHERE id = ?"
+  pokemon=db.execute(sql,id)[0]
+  return Pokemon.new(id:pokemon[0],name:pokemon[1],type:pokemon[2],db:db)
+#binding.pry
+end
+
+
 end
